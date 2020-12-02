86724cb4fd0494ab154a2f3736132ccb45f9657b
adventofandy <advent@katanasec.dev>
2020-12-02 17:56:45 -0500

    Line parsing working

---
+++ 2/advent_passwords.py
@@ -0,0 +1,19 @@
+import sys
+import re
+
+def parse_password_line(line):
+    regex = re.compile(
+            r'^(?P<min>\d*)-(?P<max>\d*) (?P<letter>\w*): (?P<password>\w*)')
+    print(line)
+    match = regex.search(line)
+    
+    result_dict = match.groupdict() 
+    print(result_dict)
+    return result_dict
+
+if __name__ == "__main__":
+    lines = None
+    with open(sys.argv[1], "rb") as f:
+        lines = f.readlines()
+    
+    assert lines
